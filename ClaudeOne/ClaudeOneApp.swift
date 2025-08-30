ffc894759517981a98bb7a4320da3b05a2d1f56f
Mark Reidy <mreidy@mac.com>
2025-08-30 13:49:20 -0400

    Initial Commit

---
+++ ClaudeOne/ClaudeOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeOneApp.swift
+//  ClaudeOne
+//
+//  Created by Mark Reidy on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClaudeOne
+//
+//  Created by Mark Reidy on 8/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
